10cbfb5c4403994c7237f8880a564e4160f58ec7
arief <arief@volantis.io>
2025-06-02 20:03:36 +0700

    chore: add postMessage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
     import "../app.css";
+    import { afterNavigate } from '$app/navigation';
+
+    afterNavigate(({ to }) => {
+        window.parent.postMessage({ pathname: to.url.pathname }, '*');
+    });
 </script>
   
 <slot />
