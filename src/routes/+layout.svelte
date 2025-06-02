ccb92a242f18900be67ff4097b0ecbb7a40baab6
arief <arief@volantis.io>
2025-06-02 20:22:20 +0700

    add type postMessage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,10 @@
     import { afterNavigate } from '$app/navigation';
 
     afterNavigate(({ to }) => {
-        window.parent.postMessage({ pathname: to.url.pathname }, '*');
+        window.parent?.postMessage(
+             { type: 'iframe:navigated', path: to.url.pathname, full: to.url.href },
+            '*'
+        );
     });
 </script>
   
